64da297e8079e2c13fad4b2cd9d3e2397033e2b7
prestonf2003 <33843946+prestonf2003@users.noreply.github.com>
2022-05-12 23:27:02 -0400

    Add project files.

---
+++ CoffeeRegistration/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Information(Registration r)
+        {
+            return View(r);
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeRegistration/Models/Registration.cs
@@ -0,0 +1,12 @@
+﻿namespace CoffeeRegistration.Models
+{
+    public class Registration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password {get; set;}
+        public string ImageUrl { get; set;}
+
+    }
+}
